0f10917f2ef14709ca198c41bbabe7a6aa0ea328
LauraB <BodellLaura@gmail.com>
2018-01-22 20:12:04 -0500

    I added properties to the Product class and added selectors for product features

--- lib/pet_toys/product.rb
+++ lib/pet_toys/product.rb
@@ -1,6 +1,8 @@
 
 class PetToys::Product 
 
+	attr_accessor :title, :price, :description 
+
 	def self.get_products 
 		self.scrape_products
 
@@ -8,8 +10,12 @@ class PetToys::Product
 
 
 	 def self.scrape_products
-	 	doc = Nokogiri::HTML(open("https://storeforthedogs.com/collections/work-to-eat-toys"))
-	 	puts doc 
+	 	doc = Nokogiri::HTML(open("https://storeforthedogs.com/collections/work-to-eat-toys/products/orbee-tuff-link"))
+	 	product = self.new 
+	 	product.title = doc.search("div.product-title h1").text.strip 
+	 	product.price = doc.search("span#ProductPrice-product-template").text.strip
+	 	product.description = doc.search("div.description.rte span").text.strip
+	 	puts product.title, product.price, product.description 
 	 end 
 
     
